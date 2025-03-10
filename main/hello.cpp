5302ce3cd6b3e46a0953174197c5031c1cd38236
PES2UG22CS009 <pes2ug22cs009@pesu.pes.edu>
2025-03-11 00:56:41 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Webhook Trigger Success!" << endl;
   return 0;
 }
